99b2581f04ca4c69509f438ba5c3830a32f9704b
Tomer <tztomer@tomers-mbp.mynet>
2022-09-11 10:47:05 +0300

    fadsf

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,18 @@
 </template>
 
 <script>
-	import appHeader from "./components/app-header.cmp.vue";
-	import appFooter from "./components/app-footer.cmp.vue";
+	// import appHeader from "./components/app-header.cmp.vue";
+	// import appFooter from "./components/app-footer.cmp.vue";
 	export default {
 		name: "main-app",
-		props: [],
-		components: {
-			appHeader,
-			appFooter,
-		},
-		created() {
-			this.$store.dispatch({ type: "loadToys" });
-		},
+		// props: [],
+		// components: {
+		// 	// appHeader,
+		// 	// appFooter,
+		// },
+		// created() {
+		// 	this.$store.dispatch({ type: "loadToys" });
+		// },
 	};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
 import '@/styles/styles.scss'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import VueGoogleMaps from '@fawmi/vue-google-maps'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 
 
 const app = createApp(App)
 
-app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyAopS6SI7oP3X0FuYHfJb_K8nEmU5jQbmI',
-    },
-})
+// app.use(VueGoogleMaps, {
+//     load: {
+//         key: 'AIzaSyAopS6SI7oP3X0FuYHfJb_K8nEmU5jQbmI',
+//     },
+// })
 
 app.use(router)
-app.use(store)
-app.use(ElementPlus)
+// app.use(store)
+// app.use(ElementPlus)
 
 app.mount('#app')
